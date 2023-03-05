d2cb6e09cc66dcdd1b5ce5e7d312e935013a4fc6
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-03-05 17:51:58 -0500

    Filters

--- components/TodoList/TodoList.jsx
+++ components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import AddTodo from "./AddTodo/AddTodo";
 import Todo from "./Todo/Todo";
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     {
       id: "123",
@@ -16,27 +16,35 @@ export default function TodoList() {
     },
   ]);
 
-  const handleAdd = (todo) => 
-    setTodos([...todos, todo]);
-  const handleUpdate = (updated) => 
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
     setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
-  const handleDelete = (deleted) => 
-    setTodos(todos.filter((t) => (t.id !== deleted.id)));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((t) => t.id !== deleted.id));
+
+  const filtered = getFilteredItems(todos, filter);
 
   return (
     <Section>
       <ul>
-        <Checkbox onChange={onChange}/>
-        {todos.map((item) => (
-          <Todo 
-            key={item.id} 
-            todo={item} 
-            onUpdate={handleUpdate} 
+        <Checkbox onChange={onChange} />
+        {filtered.map((item) => (
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
             onDelete={handleDelete}
-            />
+          />
         ))}
       </ul>
       <AddTodo onAdd={handleAdd} />
     </Section>
   );
+}
+
+function getFilteredItems(todos, filter) {
+  if (filter === "all") {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
 }
