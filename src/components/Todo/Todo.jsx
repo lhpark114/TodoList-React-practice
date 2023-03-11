0870885461c490d5d115fe4199e7f0c2c79eeefa
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-03-11 13:44:39 -0500

    npm yarn install

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaTrashAlt } from "react-icons/fa";
-import styles from './Todo.module.css'
+import styles from "./Todo.module.css";
 
 export default function Todo({ todo, onUpdate, onDelete }) {
   const { id, text, status } = todo;
@@ -13,17 +13,19 @@ export default function Todo({ todo, onUpdate, onDelete }) {
   return (
     <li className={styles.todo}>
       <input
-      className={styles.checkbox}
+        className={styles.checkbox}
         type="checkbox"
         id={id}
         checked={status === "completed"}
         onChange={handleChange}
       />
-      <label htmlFor={id} className={styles.text}>{text}</label>
+      <label htmlFor={id} className={styles.text}>
+        {text}
+      </label>
       <span className={styles.icon}>
         <button onClick={handleDelete} className={styles.button}>
-        <FaTrashAlt />
-      </button>
+          <FaTrashAlt />
+        </button>
       </span>
     </li>
   );
